e60567a285e83994e79bfabd88d6e6bbe96669be
csulit <chrisgelosulit@gmail.com>
2022-09-01 04:08:31 +0000

    Added RTK

---
+++ src/app/store/store.ts
@@ -0,0 +1,16 @@
+import counterReducer from '#rtkfeatures/counter/counter-slice';
+import { apiSlice } from '#rtkfeatures/dogs/dogs-api-slice';
+import { configureStore } from '@reduxjs/toolkit';
+
+export const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
+  },
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware().concat(apiSlice.middleware);
+  },
+});
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
