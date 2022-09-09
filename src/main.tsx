9ac789ce2b044a27a1d31ee528b2b4dbc8e50885
csulit <christian.sulit@kmc.solutions>
2022-09-09 03:04:50 +0000

    Setup auth0 in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import { store } from '#rtkstore/store';
+import { Auth0Provider } from '@auth0/auth0-react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
@@ -7,8 +8,14 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <Auth0Provider
+      domain='kmc-erp.jp.auth0.com'
+      clientId='2bYyoZLIj60Vi1MplJZGJrR4xtSyf24d'
+      redirectUri={window.location.origin}
+    >
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Auth0Provider>
   </React.StrictMode>
 );
