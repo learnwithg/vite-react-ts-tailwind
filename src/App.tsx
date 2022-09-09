3d680e79799e492330a984f60295cd6233c46e88
csulit <christian.sulit@kmc.solutions>
2022-09-09 03:35:08 +0000

    Added auth0/react login, logout btn

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import LoginButton from './components/auth0/login';
+
 function App() {
   return (
     <div>
       <p className='text-center text-red-500'>React Vite</p>
+      <LoginButton />
     </div>
   );
 }
