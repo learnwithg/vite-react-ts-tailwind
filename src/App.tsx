327fac43435c01584c7dd647a58dde8dd1d309cc
joelcabagua <cabagua.joel@gmail.com>
2022-09-14 10:03:10 +0800

    Refactor

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Login from './components/auth0/login'
 function App() {
   return (
     <div>
-      <p className="text-center text-red-500">React Vite</p>
+      <p className="text-center text-red-500">React Vites</p>
       <Login />
     </div>
   )
