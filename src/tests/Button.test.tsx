7e2a02e8cbd2bbe6bd1f6b416e53551f642cf016
Jhon Joel Cabagua Jr <cabagua.joel@gmail.com>
2022-09-21 11:06:04 +0800

    Refactor storybook.yml file

--- src/tests/Button.test.tsx
+++ src/tests/Button.test.tsx
@@ -3,8 +3,7 @@ import { render, screen } from '@testing-library/react'
 
 describe('Accordion test', () => {
   test('should show title all the time', () => {
-    render(<Button role="button">Button
-    </Button>)
+    render(<Button role="button">Button</Button>)
 
     expect(screen.getByText('Button')).toBeDefined()
   })
