7d20f48a90a75c8d95885604e605f354a03a104e
Jhon Joel Cabagua Jr <cabagua.joel@gmail.com>
2022-09-21 14:13:29 +0800

    Refactor

---
+++ storybook-static/assets/es.string.from-code-point.cd7e55c9.js
@@ -0,0 +1,2 @@
+import{_ as i,f as v,o as d}from"./iframe.8a6bf3ad.js";var f=i,s=v,g=d,m=RangeError,e=String.fromCharCode,n=String.fromCodePoint,u=s([].join),C=!!n&&n.length!=1;f({target:"String",stat:!0,arity:1,forced:C},{fromCodePoint:function(h){for(var t=[],a=arguments.length,o=0,r;a>o;){if(r=+arguments[o++],g(r,1114111)!==r)throw m(r+" is not a valid code point");t[o]=r<65536?e(r):e(((r-=65536)>>10)+55296,r%1024+56320)}return u(t,"")}});
+//# sourceMappingURL=es.string.from-code-point.cd7e55c9.js.map
