29e06663681ceb1674c9479c551a777c0e2b1ee8
Seth Maño <103634424+jSethMano@users.noreply.github.com>
2022-10-10 13:10:52 +0800

    Refactor Tabs

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -73,4 +73,3 @@ export const store = configureStore({
 
 export type AppDispatch = typeof store.dispatch
 export type RootState = ReturnType<typeof store.getState>
-// export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
